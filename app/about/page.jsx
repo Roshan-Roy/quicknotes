9b3ac5432ea6c4368ff70e86db73365a651223f5
Roshan Roy <roshanroy2911@gmail.com>
2024-03-19 23:15:10 +0530

    initial commit

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -9,7 +9,7 @@ const AboutPage = () => {
       <p>A secure note taking app built with Next.js and MongoDB. Stay organized and access your notes from anywhere!  QNotes allows you to create, edit, and delete notes with ease.  Authenticate securely using credentials or GitHub to keep your notes private using nextauth V4.</p>
       <h2>About Next JS</h2>
       <p>A popular framework built on React, streamlines the creation of modern web applications. It leverages React&apos;s power for user interfaces, but adds features like server side rendering and static site generation to boost SEO and initial load times.  Fetching data and routing are simplified, while automatic code splitting keeps your app performant. With built-in features and a vast community, Next.js empowers you to build scalable React applications efficiently.</p>
-      <h2>Devloped By</h2>
+      <h2>Developed By</h2>
       <p className={styles.developer}>Roshan Roy</p>
       <ul>
         <li><Link href="https://www.linkedin.com/in/roshan-roy-8a960b270"><FaLinkedin /></Link></li>
